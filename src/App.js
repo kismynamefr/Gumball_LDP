e77f056995c434fad781721f6d4be515216c1016
TungLee0607 <letung31071999@gmail.com>
2023-06-24 01:46:29 +0700

    add UI loading page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './Scss/main.scss'
 import Banner from './components/Banner'
 import Benefit from './components/Benefit'
+import Loading from './components/Loading'
 import Navbar from './components/Navbar'
 import Roadmap from './components/Roadmap'
 
 function App() {
   return (
     <div className="app">
+      <Loading />
       <Navbar />
       <Banner />
       <Roadmap />
