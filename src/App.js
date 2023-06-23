0b160d08cffc9cd29ac19f7c547422740c7daf56
kismynamefr <kismyname.fr@gmail.com>
2023-06-24 01:49:14 +0700

    [FE Logic] Adding aos animation

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { useEffect } from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 import './Scss/main.scss'
 import Banner from './components/Banner'
 import Benefit from './components/Benefit'
@@ -5,6 +8,17 @@ import Navbar from './components/Navbar'
 import Roadmap from './components/Roadmap'
 
 function App() {
+
+  useEffect(() => {
+    AOS.init({
+      // disable: function () {
+      //   let maxWidth = 768
+      //   return window.innerWidth <= maxWidth
+      // },
+    })
+    AOS.refresh()
+  }, [])
+
   return (
     <div className="app">
       <Navbar />
